6903b0d31504d30b4818cb758fc9d325820da362
Oğuzcan <131633350+ogiboy@users.noreply.github.com>
2024-02-09 02:48:44 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import PokeCards from "../src/component/PokeCards";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>2. Challenge: Loops</h1>
+      <PokeCards />
+    </div>
+  );
+}
---
+++ src/component/PokeCards.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { mockPokemonsData } from "../mock/pokeData";
+// import Link from "next/link";
+// import Image from "next/image";
+
+const PokeCard = () => {
+  const sortedPokesByName = mockPokemonsData.sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+  const [shinyPoke, setShinyPoke] = useState(null);
+
+  const handleMouseOver = (poke) => setShinyPoke(poke);
+  const handleMouseLeave = () => setShinyPoke(null);
+
+  return (
+    <section>
+      {sortedPokesByName.map((poke) => (
+        <article key={poke.name}>
+          <h1 style={{ textTransform: "capitalize" }}>{poke.name}</h1>
+          <a href={poke.videoLink} target="_blank" rel="noreferrer">
+            <img
+              onMouseOver={() => handleMouseOver(poke)}
+              onMouseLeave={handleMouseLeave}
+              alt={poke.name}
+              src={
+                shinyPoke === poke
+                  ? poke.sprites.front_shiny || poke.sprites.back_shiny
+                  : poke.sprites.front_default || poke.sprites.back_default
+              }
+            />
+          </a>
+        </article>
+      ))}
+    </section>
+  );
+};
+
+export default PokeCard;
